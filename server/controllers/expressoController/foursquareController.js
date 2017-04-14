16b40b0fb797d133cb2de523f4b02d48616c0ad5
nvelickovic10 <nvelickovic10@gmail.com>
2017-04-14 10:28:14 +0200

    better 4square api integration

--- server/controllers/expressoController/foursquareController.js
+++ server/controllers/expressoController/foursquareController.js
@@ -11,7 +11,6 @@ var API_VERSION = '20170101';
 var CLIENT_ID = '0VWAZA4ME5KLJOIGIR4K3II1G2BKZOPZI4PXAET30AWOYA04';
 var CLIENT_SECRET = 'JXHE3RR1UG1XOBGY0H4IRUCIRU1WCNTXL5FAFQKTGK1P4VBK';
 
-
 /**
  * Fetches data from foursquare API at given url with given parameters.
  * Secret and Id will be added to the request in this function.
@@ -62,19 +61,77 @@ var parseGetParameters = function (rawParameters) {
   return parsedParameters;
 };
 
-// /expresso/foursquare/nearCoffeeShops?ll=44.83,20.54&pretty=true
-// coffee shop category 4bf58dd8d48988d1e0931735
-foursquareController.get('/nearCoffeeShops', function (req, res) {
+/**
+ * Parses coffee shop data from raw venue data.
+ * Returns parsed coffee shop object.
+ * @param {Object} rawVenue
+ * @returns {Object}
+ */
+var parseCoffeeShop = function (rawVenue, groupName) {
+  var parsedCoffeeShop = {};
+
+  parsedCoffeeShop.id = rawVenue.id;
+  parsedCoffeeShop.name = rawVenue.name;
+  parsedCoffeeShop.distance = rawVenue.distance;
+
+  if (rawVenue.price) {
+    parsedCoffeeShop.price = rawVenue.price;
+  }
+
+  parsedCoffeeShop.categories = _.map(rawVenue.categories, function (category) {
+    return {
+      id: category.id,
+      name: category.name
+    }
+  });
+
+  if (rawVenue.featuredPhotos && rawVenue.featuredPhotos.count > 0) {
+    parsedCoffeeShop.photo = rawVenue.featuredPhotos.items[0].prefix + 'original' + rawVenue.featuredPhotos.items[0].suffix
+  }
+
+  parsedCoffeeShop.groupName = groupName;
+
+  return parsedCoffeeShop;
+};
+
+/**
+ * Parses coffee shop data from raw groups data.
+ * Returns parsed coffee shops list.
+ * @param {List} rawGroups
+ * @returns {List}
+ */
+var parseGroups = function (rawGroups) {
+  var parsedCoffeeShops = [];
+  _.each(rawGroups, function (rawGroup, groupIndex) {
+    _.each(rawGroup.items, function (item, itemIndex) {
+      var parsedCoffeeShop = parseCoffeeShop(item.venue, rawGroup.name);
+      parsedCoffeeShops.push(parsedCoffeeShop);
+    });
+  });
+  return parsedCoffeeShops;
+}
+
+foursquareController.get('/exploreCoffeeShops', function (req, res) {
+  var url = 'venues/explore';
   var parameters = parseGetParameters(req.query);
-  console.log(parameters);
-  var url = 'venues/search';
+
   var foursquareSearchParameters = {
     ll: parameters.ll,
-    query: parameters.query,
+    llAcc: parameters.accuracy,
     radius: parameters.radius,
-    categoryId: '4bf58dd8d48988d1e0931735'
+    limit: parameters.limit,
+    offset: parameters.offset,
+    sortByDistance: 'true',
+    section: 'coffee',
+    venuePhotos: 'true',
+    openNow: 'true',
+    price: '1,2,3,4'
   };
+
   fetchFoursquareDataAsJSON(url, foursquareSearchParameters, function (jsonData) {
+    var parsedCoffeeShops = parseGroups(jsonData.response.groups);
+    // console.log(JSON.stringify(parsedCoffeeShops, null, 2), parsedCoffeeShops.length);
+
     if (parameters.pretty) {
       res.send('<pre>' + JSON.stringify(jsonData, null, 2) + '</pre>');
     } else {
@@ -84,7 +141,7 @@ foursquareController.get('/nearCoffeeShops', function (req, res) {
 });
 
 foursquareController.get('/', function (req, res) {
-  res.send('foursquareController');
+  res.send('foursquareController hello');
 });
 
 module.exports = foursquareController;
